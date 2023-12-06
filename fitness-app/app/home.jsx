d3b8d145c5fdb43b0cda099ee1c4819d3b91632a
Yunus <ynstrn811@gmail.com>
2023-12-06 23:30:10 +0300

    started to develop fitness app to learn lots of libs and features such as carousel linear-gradient and responsive screens

---
+++ fitness-app/app/home.jsx
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { Ionicons } from "@expo/vector-icons";
+import ImageSlider from "../components/ImageSlider";
+
+export default function Home() {
+  return (
+    <SafeAreaView className="flex-1 flex bg-white space-y-5" edges={["top"]}>
+      <StatusBar style="dark" />
+      {/*puncline and avatar*/}
+
+      <View className="flex-row justify-between items-center mx-5">
+        <View className="space-y-2">
+          <Text
+            style={{ fontSize: hp(4.5) }}
+            className="font-bold tracking-wider text-neutral-700"
+          >
+            READY TO
+          </Text>
+          <Text
+            style={{ fontSize: hp(4.5) }}
+            className="font-bold tracking-wider text-rose-500"
+          >
+            WORKOUT
+          </Text>
+        </View>
+        <View className="flex space-y-2 justify-center items-center">
+          <Image
+            source={require("../assets/images/avatar.png")}
+            style={{ height: hp(6), width: hp(6) }}
+            className="rounded-full"
+          />
+          <View
+            className="bg-neutral-200 flex justify-center items-center rounded-full border-[3px] border-neutral-300"
+            style={{ height: hp(5.5), width: hp(5.5) }}
+          >
+            <Ionicons name="notifications" size={hp(3)} color="gray" />
+          </View>
+        </View>
+      </View>
+      {/*Image Slider*/}
+      <View>
+        <ImageSlider />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ fitness-app/app/index.jsx
@@ -0,0 +1,66 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { LinearGradient } from "expo-linear-gradient";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { useRouter } from "expo-router";
+
+export default function Index() {
+  const router = useRouter();
+
+  return (
+    <View className="bg-red-300 flex-1 justify-end">
+      <StatusBar style="light" />
+      <Image
+        className="w-full h-full absolute"
+        source={require("../assets/images/welcome.png")}
+      />
+      <LinearGradient
+        colors={["transparent", "#18181b"]}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="flex justify-end pb-12 space-y-8"
+      >
+        <Animated.View
+          entering={FadeInDown.delay(100).springify()}
+          className="flex items-center"
+        >
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Best <Text className="text-rose-500">Workouts</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            For you
+          </Text>
+        </Animated.View>
+        <Animated.View entering={FadeInDown.delay(200).springify()}>
+          <TouchableOpacity
+            onPress={() => router.push("home")}
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-rose-500 flex items-center justify-center mx-auto border-[2px] border-neutral-200 rounded-3xl"
+          >
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="text-white font-bold tracking-widest"
+            >
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </LinearGradient>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
